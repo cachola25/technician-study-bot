2d8df10956460914f9be1824923a491463be118a
cachola25 <cachola25@up.edu>
2025-09-04 10:11:57 -1000

    Skeleton chat with basic layout created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import studyBotMain from './study-bot-main'
+import StudyBotMain from './study-bot-main'
 
 function App() {
-  return studyBotMain();
+  return <StudyBotMain />;
 }
 export default App
--- src/study-bot-main.tsx
+++ src/study-bot-main.tsx
@@ -1,16 +1,19 @@
-import { INITIAL_FIRE_PROMPT, TEMP_TOPICS } from "./constants";
-
 // Main function that will handle prompts to the
 // chat bot
-export default function studyBotMain() {
-    return (
-        <div>
-            <header>Technician Study Bot</header>
-            <main>
-                <div>
-                    Chat area
-                </div>
-            </main>
+export default function StudyBotMain() {
+  return (
+    <div className="border rounded-xl bg-gray-100 flex flex-col">
+      <header className="border rounded-xl border-b p-4 font-semibold bg-blue-200">
+        Technician Study Bot
+      </header>
+
+      <main className="border rounded-xl flex items-center justify-center p-6 bg-green-200">
+        <div className="border rounded-xl p-20 text-neutral-400">
+          Chat area
         </div>
-    );
-};
+      </main>
+      
+    </div>
+    
+  );
+}
