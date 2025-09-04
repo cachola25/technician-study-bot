a5ffb4c94d2e03787b374fa68c791c850f9104b2
cachola25 <cachola25@up.edu>
2025-09-04 09:50:22 -1000

    initial skeleton chat — only text, no chat box

--- src/study-bot-main.tsx
+++ src/study-bot-main.tsx
@@ -1,1 +1,16 @@
 import { INITIAL_FIRE_PROMPT, TEMP_TOPICS } from "./constants";
+
+// Main function that will handle prompts to the
+// chat bot
+export default function studyBotMain() {
+    return (
+        <div>
+            <header>Technician Study Bot</header>
+            <main>
+                <div>
+                    Chat area
+                </div>
+            </main>
+        </div>
+    );
+};
